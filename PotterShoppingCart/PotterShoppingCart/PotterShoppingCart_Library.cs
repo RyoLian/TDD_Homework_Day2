72032e10edd183804d72e7c10da75992e037f2cb
Ryo <Georgpeck_0715@yahoo.com.tw>
2016-07-14 11:30:04 +0800

    一次買了整套 <綠燈>

--- PotterShoppingCart/PotterShoppingCart/PotterShoppingCart_Library.cs
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCart_Library.cs
@@ -2,15 +2,15 @@
 {
     public class PotterShoppingCart
     {
-        private int[] _FeeRate = new int[] { 100, 95, 90, 80 };
+        private int[] _FeeRate = new int[] { 100, 95, 90, 80, 75 };
 
         public int CalTotalPay(int First, int Second, int third, int forth, int fifth)
         {
             var TotalPay = 0;
 
-            int[] list = { First, Second, third, forth };
+            int[] list = { First, Second, third, forth, fifth };
             int kind = 0;
-            var TotalCount = First + Second + third + forth;
+            var TotalCount = First + Second + third + forth + fifth;
 
             for (int i = 0; i <= list.Length - 1; i++)
             {

4de9e7131ec742c5108bbf2979cab723c1cb8d30
Ryo <Georgpeck_0715@yahoo.com.tw>
2016-07-14 09:35:21 +0800

    只買第一集1本 尚未實作ProductionCode<紅燈>

---
+++ PotterShoppingCart/PotterShoppingCart/PotterShoppingCart_Library.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart_Library
+{
+    public class PotterShoppingCart
+    {
+        public int CalTotalPay()
+        {
+            var TotalPay = 0;
+            return TotalPay;
+        }
+    }
+}
--- PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
+++ PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart_Library;
 
 namespace PotterShoppingCartTests
 {
@@ -7,8 +8,18 @@ namespace PotterShoppingCartTests
     public class PotterShoppingCartTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PotterShoppingCartTests_第一集買了一本_其他都沒買_價格應為_100元()
         {
+            //arrange
+            var Buy = new PotterShoppingCart();
+
+            //act
+            var actual = Buy.CalTotalPay();
+
+            //assert
+            var expected = 100;
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }

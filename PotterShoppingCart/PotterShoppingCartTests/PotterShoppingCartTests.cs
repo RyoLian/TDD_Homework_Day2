21c2bc1439b371725bd8d42c9b0a79332e5083c8
Ryo <Georgpeck_0715@yahoo.com.tw>
2016-07-14 09:18:03 +0800

    初始化環境

---
+++ PotterShoppingCart/PotterShoppingCartTests/PotterShoppingCartTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCartTests
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
